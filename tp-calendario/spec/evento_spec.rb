511ccdfe4fead3d99c242d7a750f71249c49864d
Rodrigo <rodrigo@localhost.localdomain>
2017-06-06 06:21:58 -0300

    Se agregan test para setear fecha_inicio y fecha_fin funcionando OK

--- tp-calendario/spec/evento_spec.rb
+++ tp-calendario/spec/evento_spec.rb
@@ -11,33 +11,53 @@ describe 'Evento' do
 
   it 'deberia crearse con nombre_evento "aydoo2017"' do
   	valor_esperado = "aydoo2017"
-	expect(@evento.nombre_evento).to eq(valor_esperado)
+  	valor_obtenido = @evento.nombre_evento
+	expect(valor_obtenido).to eq(valor_esperado)
   end
 
   it 'deberia crearse con id_evento "1"' do
   	valor_esperado = "1"
-	expect(@evento.id_evento).to eq(valor_esperado)
+  	valor_obtenido = @evento.id_evento
+	expect(valor_obtenido).to eq(valor_esperado)
   end
 
 
   it 'deberia crearse con nombre_calendario "calendario2017"' do
   	valor_esperado = "calendario2017"
-	expect(@evento.nombre_calendario_evento).to eq(valor_esperado)
+  	valor_obtenido = @evento.nombre_calendario_evento
+	expect(valor_obtenido).to eq(valor_esperado)
   end
 
   it 'deberia crearse con recurrencia "diaria"' do
   	valor_esperado = "diaria"
+  	valor_obtenido = @evento.recurrecia_evento
 	expect(@evento.recurrecia_evento).to eq(valor_esperado)
   end
 
   it 'deberia crearse con inicio_evento "2017-03-31T18:00:00-03:00"' do
   	valor_esperado = "2017-03-31T18:00:00-03:00"
-	expect(@evento.inicio_evento).to eq(valor_esperado)
+  	valor_obtenido = @evento.inicio_evento
+	expect(valor_obtenido).to eq(valor_esperado)
   end
 
   it 'deberia crearse con fin_evento "2017-03-31T22:00:00-03:00"' do
   	valor_esperado = "2017-03-31T22:00:00-03:00"
-	expect(@evento.fin_evento).to eq(valor_esperado)
+  	valor_obtenido = @evento.fin_evento
+	expect(valor_obtenido).to eq(valor_esperado)
+  end
+
+  it 'deberia poder modificarse inicio_evento "2017-03-31T19:00:00-03:00"' do
+  	@evento.set_inicio("2017-03-31T19:00:00-03:00")
+  	valor_esperado = "2017-03-31T19:00:00-03:00"
+  	valor_obtenido = @evento.inicio_evento
+	expect(valor_obtenido).to eq(valor_esperado)
+  end
+
+  it 'deberia poder modificarse fin_evento "2017-03-31T23:00:00-03:00"' do
+  	@evento.set_fin("2017-03-31T23:00:00-03:00")
+  	valor_esperado = "2017-03-31T23:00:00-03:00"
+  	valor_obtenido = @evento.fin_evento
+	expect(valor_obtenido).to eq(valor_esperado)
   end
 
  end
